c30f4194b8c8b25f47f47e0d1ed562db783a95ff
Ben <82054863+Creatorben@users.noreply.github.com>
2023-03-06 22:29:43 +0100

    Display Products complete

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -1,1 +1,22 @@
-console.log('filters project');
+let allProducts = products;
+
+const productsContainer = document.querySelectorAll('.products-container')[1];
+
+const productsData = allProducts
+  .map((product) => {
+    console.log(product);
+    const { company, image, price, title } = product;
+
+    return `<article class="product">
+  <img src="${product.image}" alt="${product.title}" class="product-img img">
+  <footer>
+    <h5 class="product-name">${product.title}</h5>
+    <span class="product-price">${product.price}</span>
+  </footer>
+</article>`;
+  })
+  .join('');
+
+console.log(productsContainer);
+
+productsContainer.innerHTML = productsData;
