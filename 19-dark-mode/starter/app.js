6c975c6b404a1e77bcba3a48d7a786f68d3224ea
Ben <82054863+Creatorben@users.noreply.github.com>
2023-03-06 22:08:55 +0100

    Finishes project

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,1 +1,27 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
+
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-theme');
+});
+
+const articlesData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+    //   format date
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+
+    return `<article class="post">
+    <h2>${article.title}</h2>
+    <div class="post-info">
+      <span>${formatDate}</span>
+      <span>${article.length} min read</span>
+    </div>
+    ${article.snippet}
+  </article>`;
+  })
+  .join('');
+
+console.log(articlesData);
+
+articlesContainer.innerHTML = articlesData;
