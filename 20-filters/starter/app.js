631c21698d80b79152b21b40564bf0ec5d0cabc5
Ben <82054863+Creatorben@users.noreply.github.com>
2023-03-07 21:58:58 +0100

    finished project

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -1,22 +1,69 @@
-let allProducts = products;
+let filteredProducts = [...products];
 
 const productsContainer = document.querySelectorAll('.products-container')[1];
 
-const productsData = allProducts
-  .map((product) => {
-    console.log(product);
-    const { company, image, price, title } = product;
-
-    return `<article class="product">
+displayProducts = () => {
+  if (filteredProducts.length < 1) {
+    return (productsContainer.innerHTML = `No results for this search...`);
+  }
+  productsContainer.innerHTML = filteredProducts
+    .map((product) => {
+      return `<article class="product">
   <img src="${product.image}" alt="${product.title}" class="product-img img">
   <footer>
     <h5 class="product-name">${product.title}</h5>
     <span class="product-price">${product.price}</span>
   </footer>
 </article>`;
-  })
-  .join('');
+    })
+    .join('');
+};
 
-console.log(productsContainer);
+displayProducts();
 
-productsContainer.innerHTML = productsData;
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+searchInput.addEventListener('keyup', () => {
+  const inputValue = searchInput.value;
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  displayProducts();
+});
+
+const companies = document.querySelector('.companies');
+
+displayButtons = () => {
+  // const uniqueCompanies = new Set();
+  // const allCompanies = products.map((entry) =>
+  //   uniqueCompanies.add(entry.company)
+  // );
+  const uniqueCompanies = [
+    'all',
+    ...new Set(products.map((product) => product.company)),
+  ];
+  console.log(uniqueCompanies);
+  companies.innerHTML = uniqueCompanies
+    .map((company) => {
+      return `<button class="company-btn" data-id="${company}">${company}</button>`;
+    })
+    .join('');
+};
+
+displayButtons();
+
+companies.addEventListener('click', (e) => {
+  const el = e.target;
+  if (el.classList.contains('company-btn')) {
+    if (el.dataset.id === 'all') {
+      filteredProducts = [...products];
+    } else {
+      filteredProducts = products.filter((product) => {
+        return product.company === el.dataset.id;
+      });
+    }
+    searchInput.value = '';
+    displayProducts();
+  }
+});
